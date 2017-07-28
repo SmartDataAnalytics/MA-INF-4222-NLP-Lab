760ace6867582e8d4dddd85d14388be29a7717e8
omid najaee <o.najaee@gmail.com>
2017-07-28 18:39:08 +0200

    set default tab to text

--- horus-web/src/App.js
+++ horus-web/src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			selected_tab: 'images',
+			selected_tab: 'text',
 			terms: [],
 			text_results: [],
 			image_results: []
