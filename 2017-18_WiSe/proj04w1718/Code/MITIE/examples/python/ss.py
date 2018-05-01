78f9c1a2648f67fb8a0f055e9715959f414fa626
aikira7 <aikira7@web.de>
2018-05-01 16:51:40 +0200

    Test

--- 2017-18_WiSe/proj04w1718/Code/MITIE/examples/python/ss.py
+++
@@ -1,8 +0,0 @@
-with open('../../emerging.test.conll', 'r') as input:
-	with open("newfile.txt","wb") as output: 
-		for line in input:
-			s = (line.rstrip('\n')).split('\t')
-			if(s[0]!=''):
-				if ((s[1]!='B-group')and(s[1]!='I-group')and(s[1]!='B-creative-work')and(s[1]!='I-creative-work')and(s[1]!='B-product')and(s[1]!='I-product')):
-					output.write(line)
-                
